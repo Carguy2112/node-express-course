fe628ddbc8388a19f1c85b8f43cb23d587490022
Tom Boyd <carguy2112@yahoo.com>
2024-01-15 15:51:44 -0600

    express basics

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,11 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express()
+const {products} = require('./data')
+
+app.get('/', (req,res)=>{
+    res.json(products)
+})
+
+app.listen(5000, ()=>{
+    console.log('server is listening on port 5k')
+})
